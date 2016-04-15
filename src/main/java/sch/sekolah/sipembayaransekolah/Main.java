ff9cefd0ab9ecbca09fb78aa3e976a93cc1bd009
Administrator <Administrator@anonymous>
2016-04-16 06:42:06 +0700

    Membuat Project

---
+++ src/main/java/sch/sekolah/sipembayaransekolah/Main.java
@@ -0,0 +1,19 @@
+/*
+ * <agus.suhardii@gmail.com>
+ */
+package sch.sekolah.sipembayaransekolah;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
